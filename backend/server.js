3b71659825f8e1f961fdfd156cafcccac9386843
dp100392vim <dp100392vim@gmail.com>
2025-07-03 13:17:05 +0300

    2

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 const app = express()
-app.use(cors())
+app.use(cors({ origin: 'https://your-frontend.onrender.com' }));
 app.use(express.json())
 
 const entryRoutes = require('./routes/entryRoutes')
@@ -13,12 +13,6 @@ app.use('/entry', entryRoutes)
 const PORT = 3000
 
 // Connect to DB
-mongoose.connect('mongodb://localhost:27017/roest')
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-    })
-  })
-  .catch(error => {
-    console.error('Database connection error:', error)
-  })
+
+const MONGODB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSW}@cluster0.m8jonut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
